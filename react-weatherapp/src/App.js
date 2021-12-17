dac9d211085daa12a15c4ef6d031283d34a81828
Isaias Quintanilla <quintanillaisaias@gmail.com>
2021-12-16 23:32:04 -0600

    Created Navbar.

--- react-weatherapp/src/App.js
+++ react-weatherapp/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../src/screens/Home";
 import './App.css';
 
 function App() {
-  return
-
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-weatherapp/src/components/navbar/nav.js
@@ -0,0 +1,14 @@
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function Navigation() {
+    return (
+        <Container>
+            <Navbar expand="lg" variant="light" bg="dark">
+                <Container>
+                    <Navbar.Brand href="#">Weather App</Navbar.Brand>
+                </Container>
+            </Navbar>
+        </Container>
+    );
+}
