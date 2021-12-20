47a5a73322c396c1ed46c7c83b30d8fa135c3cab
Isaias Quintanilla <quintanillaisaias@gmail.com>
2021-12-19 23:40:50 -0600

    Called weather api, added state hooks, rendered current weather and five day forecast cards.

--- react-weatherapp/src/components/cards/5day.js
+++ react-weatherapp/src/components/cards/5day.js
@@ -3,28 +3,27 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
 
-export default function Cards(props) {
-    let today = new Date().toDateString();
+export default function Forecast(props) {
+
     function convertTemperature(temperature) {
         let result = (temperature - 273.15) * 9 / 5 + 32;
         return result.toFixed(2);
     }
     return (
+
         <Col className="entireCard">
-            <Card className="cardsCenter" style={{ width: "30rem" }}>
+            <Card className="cardsCenter">
                 <Card.Body>
-                    <Card.Title>{props.city}</Card.Title>
+
                     <Card.Text>
-                        {today} <img id="weather-img" src={`https://api.openweathermap.org/img/w/${props.image}.png`} alt="weather icon" />
+                        {props.date} <img id="weather-img" src={`https://api.openweathermap.org/img/w/${props.image}.png`} alt="weather icon" />
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
                     <ListGroupItem>Temperature: {convertTemperature(props.temperature)} °F</ListGroupItem>
-                    <ListGroupItem>Humidity: {props.humidity} %</ListGroupItem>
-                    <ListGroupItem>Wind Speed: {props.windspeed} mph</ListGroupItem>
                 </ListGroup>
                 <Card.Body>
-                    <Card.Link href="https://openweathermap.org/api">API provided by: OpenWeather</Card.Link>
+                    {/* <Card.Link href="https://openweathermap.org/api">API provided by: OpenWeather</Card.Link> */}
                 </Card.Body>
             </Card>
         </Col>
--- react-weatherapp/src/screens/Home.js
+++ react-weatherapp/src/screens/Home.js
@@ -8,19 +8,83 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Cards from "../components/cards/cards";
+import Forecast from "../components/cards/5day";
+import Alert from "react-bootstrap/Alert";
 import axios from "axios";
 
 export default function Home() {
     const [searchCity, setSearchCity] = useState("");
     const [toggleCards, setTogggleCards] = useState(false);
+    const [temperature, setTemperature] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+    const [windSpeed, setWindSpeed] = useState(0);
+    const [image, setImage] = useState("");
+    const [fiveDayArray, setFiveDayArray] = useState([]);
+    const [dates, setDates] = useState([]);
+    const [error, setError] = useState({});
+    const [country, setCountry] = useState("");
+    const [search, setSearch] = useState("");
+    let MYSECRETKEY = "b397225e3e6e7043cc26284176c744bd";
+    let baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity},${country}&APPID=${MYSECRETKEY}`;
+    let fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity},${country}&APPID=${MYSECRETKEY}`;
 
     const handleChange = (event) => {
-        setSearchCity(event.target.value);
+        setSearch(event.target.value);
     };
 
     const submit = () => {
-        axios.get(searchCity).then(function (response) {
+        setSearchCity(search.split(", ")[0]);
+        setCountry(search.split(", ")[1]);
+        axios.get(baseUrl).then(function (response) {
+            setError({});
+            setTemperature(response.data.main.temp);
+            setHumidity(response.data.main.humidity);
+            setWindSpeed(response.data.wind.speed);
+            setImage(response.data.weather[0].icon);
+            console.log(response.data);
         })
+            .catch(function (error) {
+                if (error.response) {
+                    setTemperature(0);
+                    setHumidity(0);
+                    setWindSpeed(0);
+                    setImage("");
+                    setError(error.response.data.message);
+                }
+                console.log(error.response.data.message);
+            });
+        axios.get(fiveDayUrl).then(function (response) {
+
+            console.log(response.data.list);
+
+            let dayCount = [];
+            let fiveDay = [];
+            for (let i = 0; i < response.data.list.length; i++) {
+                let dateandtime = response.data.list[i].dt_txt;
+                let date = dateandtime.split(" ")[0];
+                let time = dateandtime.split(" ")[1];
+
+                if (time === "15:00:00") {
+                    fiveDay.push(response.data.list[i]);
+                    let year = date.split("-")[0];
+                    let month = date.split("-")[1];
+                    let day = date.split("-")[2];
+                    dayCount.push(month + "/" + day + "/" + year);
+                }
+            }
+            setDates(dayCount);
+            console.log(dayCount);
+            setFiveDayArray(fiveDay);
+            console.log(fiveDay);
+        })
+            .catch(function (error) {
+                if (error.response) {
+                    setDates([]);
+                    setFiveDayArray([]);
+                    setError(error.response.data.message);
+                }
+                console.log(error);
+            })
         setTogggleCards(true);
     }
 
@@ -31,11 +95,11 @@ export default function Home() {
                 <h3 className="center weather">Weather App</h3>
                 <Row className="center">
                     <Col md={4}>
-                        <Form.Label>Type city name to find weather forecast</Form.Label>
+                        <Form.Label>Type city name, country code to find weather forecast</Form.Label>
                         <InputGroup className="mb-3">
                             <FormControl
-                                placeholder="Type City"
-                                value={searchCity}
+                                placeholder="City, Country Code"
+                                value={search}
                                 onChange={handleChange}
                             />
                             <Button variant="dark" id="button-addon2" onClick={submit}>
@@ -44,9 +108,30 @@ export default function Home() {
                         </InputGroup>
                     </Col>
                 </Row>
-                <Row className="center">
-                    {toggleCards && <Cards word={searchCity} />}
-                </Row>
+                {Object.keys(error).length > 0 && (
+                    <Row className="error cardsCenter">
+                        <Alert key={1} variant="danger" className="center">
+                            <p>{error}<br />{"Please type a valid city."}</p>
+                        </Alert>
+                    </Row>
+                )}
+
+                {Object.keys(error).length === 0 && (<>
+                    <Row className="center">
+                        {toggleCards && <Cards city={searchCity} image={image} temperature={temperature} humidity={humidity} windspeed={windSpeed} />}
+                    </Row>
+                    <Row>
+                        {toggleCards && fiveDayArray.length > 0 && <Forecast city={searchCity} date={dates[0]} image={fiveDayArray[0].weather[0].icon} temperature={fiveDayArray[0].main.temp} />}
+
+                        {toggleCards && fiveDayArray.length > 0 && <Forecast city={searchCity} date={dates[1]} image={fiveDayArray[1].weather[0].icon} temperature={fiveDayArray[1].main.temp} />}
+
+                        {toggleCards && fiveDayArray.length > 0 && <Forecast city={searchCity} date={dates[2]} image={fiveDayArray[2].weather[0].icon} temperature={fiveDayArray[2].main.temp} />}
+
+                        {toggleCards && fiveDayArray.length > 0 && <Forecast city={searchCity} date={dates[3]} image={fiveDayArray[3].weather[0].icon} temperature={fiveDayArray[3].main.temp} />}
+
+                        {toggleCards && fiveDayArray.length > 0 && <Forecast city={searchCity} date={dates[4]} image={fiveDayArray[4].weather[0].icon} temperature={fiveDayArray[4].main.temp} />}
+                    </Row>
+                </>)}
             </Container>
         </div>
     );
